a8a5854110431264fbe14e3c1cc5c757ca976e31
Joshua Whelan <josh25whelan@gmail.com>
2020-10-26 16:39:31 -0400

    second commit

--- index.js
+++ index.js
@@ -14,24 +14,23 @@ function watchForm() {
   }
 
   function checkNumber(numberI,stateI){
+    $('.errorsec').empty()
     let numRet = 10
     if(numberI === false || numberI<=0 || numberI>300){}else{numRet = numberI}
-    console.log('numRet is ' + numRet)
     if(stateI === ''||stateI === false){
-        renderError("State cannot be blank")
+        renderError(`State cannot be blank`)
         throw new Error("State cannot be blank")
-    }
+    }else{
+        // getParks(numRet,StateI)
+    }}
 
     function renderError (err){
-        $('errorsec').html(`<div>${err}</div>`)
+        $('.errorsec').html(`<div>${err}</div>`)
     }
 
+    function getParks (maxNumber,stateInput){
 
-   // getParks(numRet)
     }
-
-
-
 
 
 $(start())
